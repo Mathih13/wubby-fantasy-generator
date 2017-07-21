633a098d883c26d2457ffe340229f6ebaebb508b
Mathih13 <mathih13@uia.no>
2017-07-21 16:09:20 +0200

    ..

--- wubby-fantasy-generator/src/components/SingleCard.js
+++ wubby-fantasy-generator/src/components/SingleCard.js
@@ -53,13 +53,11 @@ class SingleCard extends Component {
   render() {
     return (
       <div className="App">
-        <head>
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@flickr" />
-          <meta name="twitter:title" content="Small Island Developing States Photo Submission" />
-          <meta name="twitter:description" content="View the album on Flickr." />
-          <meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
-        </head>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
